c67dc82d6ff8941973d18267f2e3cd2ccf1602d1
Olexandr Stecuk <sandrostecuk@gmail.com>
2018-04-03 21:50:24 +0300

    v 1.0.3

---
+++ app/Album.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Album extends Model
+{
+    protected $table = 'albums';
+
+    public function photos(){
+    	return $this->hasMany('App\Photo');
+    }
+
+}
